8130572b2b32d33060988f69094e7abfb7ee084b
Carlos Roque <carlos.roque@bayphoto.com>
2018-05-08 17:34:36 -0500

    Added new event listener for cnf-add-entry. using JS style for var names. standardizing var names to match new attribute names.

--- app/assets/javascripts/cool_nested_forms.js
+++ app/assets/javascripts/cool_nested_forms.js
@@ -1,4 +1,42 @@
 $(function(){
+  $(document).delegate('.cnf-add-entry','click', function() {
+    // initialize options
+    var association = $(this).attr('data-association');
+    var jsTemplateName = $(this).attr('data-js-template-name');
+    var childTemplates = $(this).attr('data-child-js-template-names').split(' ');
+    var target = $(this).attr('data-target');
+
+    // initialize regular expressions
+    var associationNewRegEx = new RegExp('new_' + association, 'g');
+    var tempIdRegEx = new RegExp('\\[tempid\\]', 'g');
+    var childTempIdRegEx = new RegExp('_tempid_', 'g');
+    // var regexp_parent_new_field = new RegExp('new_' + association, 'g');
+
+    var newId = new Date().getTime();
+
+    var Dest = (target == '') ? $(this).parent() : $('#'+target);
+
+    Dest.append(window[ jsTemplateName ].replace(associationNewRegEx, newId).replace(tempIdRegEx, newId));
+    for(var index in childTemplates)
+    {
+      childTemplate = childTemplates[index];
+      if(childTemplate != "")
+      {
+        body = window[childTemplate];
+        child_script_tag = "<script> var " + childTemplate + " = '" + body + "'  </script>";
+        child_script_tag = child_script_tag.replace(childTempIdRegEx, "_" + newId + "_")
+                            .replace(associationNewRegEx, newId);
+        Dest.append(child_script_tag.replace(childTempIdRegEx, "_" + newId + "_"));
+      }
+    }
+    $(document).trigger('coolNestedForms.entryAdded');
+    return false;
+  });
+
+
+});
+
+$(function(){
   $(document).delegate('.add_child','click', function() {
     var association = $(this).attr('data-association');
     var template = $(this).attr('data-association-template');

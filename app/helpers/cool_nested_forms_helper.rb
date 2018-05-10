f0ed8beffbfce0c15acdf6bbcdd29f5182addcc4
Carlos Roque <carlos.roque@bayphoto.com>
2018-05-10 16:08:43 -0500

    Added new helper to generate the entries container with any existing entries

--- app/helpers/cool_nested_forms_helper.rb
+++ app/helpers/cool_nested_forms_helper.rb
@@ -1,5 +1,45 @@
 module CoolNestedFormsHelper
+  ## entries_container ##
+  # arguments
+  # - builder: a FormBuilder Object obtained from form_with or form_for
+  # - association: a model class i.e. MyTask
+  # - current_entries: all the current entries for the parent i.e. a Form.tasks
+  # - options: hash of override options
+  #   - partial: name of the erb partial to use as a tempalte for the existing entries
+  #   - id: ID to be used in for the container
+  #   - plurilized: the plural version of the model name
+  #   - singularized: the singular version of the model name
+  #   - parent_singularized: the name of the parent record. used for generating the entry's container id
+  #   - parent_id: the id of the parent record. used for generating the entry's container id
+  #   - class: a string with any css class names to be used for styling
+  #   - style: a string with any css styles
+  #   - tag: override the returned tag
+  def entries_container(builder,association,current_entries, options = {})
+    # initialize options
+    options[:partial] ||= "#{association.name.underscore}_edit"
+    options[:id] ||= association.name.underscore.pluralize
+    options[:pluralized] ||= association.name.underscore.pluralize
+    options[:singularized] ||= association.name.underscore
+    options[:parent_singularized] ||= builder.object.class.name.underscore
+    options[:parent_id] ||= builder.object.id.to_s
+    options[:class] ||= ''
+    options[:style] ||= ''
+    options[:tag] ||= :div
 
+    # render all the current entries
+    output = builder.fields_for(options[:pluralized], current_entries) do |assoc_builder|
+      id = "#{options[:parent_singularized]}_#{options[:parent_id]}_#{options[:singularized]}_#{assoc_builder.object.id}"
+      render options[:partial], :builder => assoc_builder, :id => id
+    end
+
+    return content_tag(
+      options[:tag],
+      output.html_safe,
+      :style => options[:style],
+      :class => options[:class],
+      :id => options[:id]
+    )
+  end
   ## new_entry_template ##
   # arguments
   # - builder: a FormBuilder Object obtained from form_with or form_for
@@ -12,7 +52,7 @@ module CoolNestedFormsHelper
   #   - children: an array with childre association options for nested association of a nested association
   def new_entry_template(builder,association, options = {})
     # initialize options
-    options[:partial] ||= association.name.underscore
+    options[:partial] ||= "#{association.name.underscore}_new"
     options[:id] ||= "#{association.name.underscore}_[tempid]"
     options[:pluralized] ||= association.name.underscore.pluralize
     options[:js_template_name] ||= association.name.underscore.pluralize
